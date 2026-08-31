607b1754fc517e9f0c4db2118aaf7c65dca13ac6
ayomidekareempau <ayomide.kareem@pau.edu.ng>
2026-08-31 21:05:37 +0100

    fixed some errors

--- week-2/projects/project_1.rs
+++ week-2/projects/project_1.rs
@@ -1,10 +1,10 @@
 fn main() {
-	let p: f32 = 520000000.0;
+	let p:f32 = 520000000.0;
 	let r:f32 = 10.0;
 	let n:f32 = 5.0;
 
-	let a = p * (1.0 + (r / 100.0)) * n;
+	let a = p * (1.0 + (r / 100.0)).powf(n) ;
 	let ci = a - p;
-	println!("The Computnd Interest is {}", ci);
+	println!("The Compound Interest is {}", ci);
 }
 
---
+++ week-2/projects/project_3.rs
@@ -0,0 +1,3 @@
+let cost:f32 = 210000.0;
+let r:f32 = 5.0;
+let n:f32 = 
