a5491c6db3b5a19fadae69ce3a2a8d049af4f881
ayomidekareempau <ayomide.kareem@pau.edu.ng>
2026-08-31 19:16:30 +0100

    added first project

---
+++ week-2/projects/project_1.rs
@@ -0,0 +1,10 @@
+fn main() {
+	let p: f32 = 520000000.0;
+	let r:f32 = 10.0;
+	let n:f32 = 5.0;
+
+	let a = p * (1.0 + (r / 100.0)) * n;
+	let ci = a - p;
+	println!("The Computnd Interest is {}", ci);
+}
+
