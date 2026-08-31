45f63a9b7fcaf2743f4fa57ac691352ca06827f7
ayomidekareempau <ayomide.kareem@pau.edu.ng>
2026-08-31 20:41:15 +0100

    added project2 file

---
+++ week-2/projects/project_2.rs
@@ -0,0 +1,10 @@
+fn main() {
+	let sum:f32 = 450000.0 + 1500000.0 + 750000.0 + 2850000.0 + 250000.0;
+	let average:f32 = sum / 5.0;
+
+	println!("The sum of the sales records are: {}", sum );
+	println!("The average of the sales records are: {}", average );
+
+
+
+}
