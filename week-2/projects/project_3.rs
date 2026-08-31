d4093c5d7d08dcdceae0fe954c2277f40fba6944
ayomidekareempau <ayomide.kareem@pau.edu.ng>
2026-08-31 21:16:44 +0100

    added new files

--- week-2/projects/project_3.rs
+++ week-2/projects/project_3.rs
@@ -1,3 +1,10 @@
-let cost:f32 = 210000.0;
-let r:f32 = 5.0;
-let n:f32 = 
+fn main() {
+	let p:f32 = 210000.0;
+	let r:f32 = 5.0;
+	let n:f32 = 3.0;
+
+	let a:f32 = p * (1.0 - (r / 100.0 )).powf(n);
+	println!("The value of the TV has deprecated to: {}", a);
+
+}
+
